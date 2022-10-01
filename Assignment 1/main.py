b6160c60febeb29c1caa01b2e557297fa13eb433
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-10-01 20:02:45 +0530

    Delete Assignment 1 directory

--- Assignment 1/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import ibm_db
-import urllib.parse
-from flask_cors import CORS
-
-app=Flask(__name__)
-cors=CORS(app)
--- Assignment 1/run.py
+++
@@ -1,4 +0,0 @@
-from main import *
-from router import *
-if __name__=="__main__":
-    app.run(port=3000,debug=True)
