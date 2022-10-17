89d1f9d97a18597ec75f4d36bd38e6f7ba35240b
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-10-17 12:12:59 +0530

    Add: Assignment 5

--- Assignments/Naveenkumar/Assignment-3/assign5.py
+++ Assignments/Naveenkumar/Assignment-3/assign5.py
@@ -7,7 +7,7 @@ def index():
 @app.route('/setcookie', methods = ['POST', 'GET'])
 def setcookie():
    if request.method == 'POST':
-   user = request.form['nm']
+      user = request.form['nm']
    
    resp = make_response(render_template('readcookie.html'))
    resp.set_cookie('userID', user)
