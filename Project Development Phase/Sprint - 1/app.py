7ee219d980d6ceca4c7f346ddc4a330a30af6481
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-11-16 22:05:42 +0530

    project report

--- Project Development Phase/Sprint - 1/app.py
+++ Project Development Phase/Sprint - 1/app.py
@@ -9,7 +9,6 @@ import win32api
 app = Flask(__name__)
 
 
-
 conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30699;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gpf28409;PWD=G9IH9SYk8j8QnHJi;", "", "")
 
 #Index
